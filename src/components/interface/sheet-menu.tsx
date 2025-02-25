9a0aeaedbfd24750462b58d1356c3243d61735fa
Juanmorales1810 <juan1810.jm@gmail.com>
2025-02-25 10:01:13 -0300

    Feat: Agregar componente SheetMenu para mejorar la navegación en la interfaz

---
+++ src/components/interface/sheet-menu.tsx
@@ -0,0 +1,115 @@
+import { Button } from "@/components/ui/button"
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+} from "@/components/ui/sheet"
+import { useState } from "react";
+
+export function SheetMenu() {
+    const [open, setOpen] = useState(false);
+    return (
+        <Sheet open={open} onOpenChange={setOpen}>
+            <SheetTrigger asChild>
+                <Button variant="outline">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="size-5"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M4 6h16M4 12h16M4 18h16"></path>
+                    </svg>
+                </Button>
+            </SheetTrigger>
+            <SheetContent>
+                <SheetHeader>
+                    <SheetTitle>Proviser - Chile</SheetTitle>
+                    <SheetDescription>
+
+                    </SheetDescription>
+                </SheetHeader>
+                <nav aria-label="Global">
+                    <ul className="flex flex-col items-start font-bold gap-6 text-xl">
+                        <li>
+                            <a
+                                className="text-zinc-900 transition hover:text-zinc-900/75 font-semibold"
+                                onClick={() => setOpen(false)}
+                                href="/#Hero"
+                            >
+                                Inicio
+                            </a>
+                        </li>
+
+                        <li>
+                            <a
+                                className="text-zinc-900 transition hover:text-zinc-900/75 font-semibold"
+                                onClick={() => setOpen(false)}
+                                href="/#Sectores"
+                            >
+                                Sectores
+                            </a>
+                        </li>
+
+                        <li>
+                            <a
+                                className="text-zinc-900 transition hover:text-zinc-900/75 font-semibold"
+                                onClick={() => setOpen(false)}
+                                href="/#Servicios"
+                            >
+                                Servicios y productos
+                            </a>
+                        </li>
+
+                        <li>
+                            <a
+                                className="text-zinc-900 transition hover:text-zinc-900/75 font-semibold"
+                                onClick={() => setOpen(false)}
+                                href="/#Sobre-nosotros"
+                            >
+                                Sobre nosotros
+                            </a>
+                        </li>
+
+                        <li>
+                            <a
+                                className="text-zinc-900 transition hover:text-zinc-900/75 font-semibold"
+                                onClick={() => setOpen(false)}
+                                href="#"
+                            >
+                                Productos
+                            </a>
+                        </li>
+
+                        <li>
+                            <a
+                                className="text-zinc-900 transition hover:text-zinc-900/75 font-semibold"
+                                onClick={() => setOpen(false)}
+                                href="/#Testimonios"
+                            >
+                                Testimonios
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                className="text-zinc-900 transition hover:text-zinc-900/75 font-semibold"
+                                onClick={() => setOpen(false)}
+                                href="/#Contacto"
+                            >
+                                Contacto
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </SheetContent>
+        </Sheet>
+    )
+}
